05ffbb4d1bab181512925c84c0f80e5e80f51e35
Zekaria Mohammed <zakisu250@gmail.com>
2023-12-09 14:00:55 +0300

    Update API URLs in constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,8 @@
 export const BASE_URL = '';
-export const PRODUCTS_URL = '/products';
-export const USERS_URL = '/users';
-export const ORDERS_URL = '/orders';
-export const PAYPAL_URL = '/config/paypal';
-export const UPLOAD_URL = '/upload';
+export const PRODUCTS_URL = '/api/products';
+export const USERS_URL = '/api/users';
+export const ORDERS_URL = '/api/orders';
+export const PAYPAL_URL = '/api/config/paypal';
+export const UPLOAD_URL = '/api/upload';
 // export const TOKEN =
 //   'A21AAK0B1uZrSe37pZAWgSkb8mjZlrBJa71iq0XuMm-ks2OEakL-jzViHkxAE43W-e1W8No92T1KH7SjHOE2Jkju9-dhO_phQ';
