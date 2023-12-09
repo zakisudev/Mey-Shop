a4870a541ed3c2a60c23cdc755d6c2ce73e165e5
Zekaria Mohammed <zakisu250@gmail.com>
2023-12-09 12:37:59 +0300

    TODO for TeleBirr

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -257,27 +257,7 @@ const OrderScreen = () => {
                       Test Pay Order
                     </Button>
 
-                    <form
-                      action="https://www.paypal.com/cgi-bin/webscr"
-                      method="post"
-                      target="_top"
-                    >
-                      <input type="hidden" name="cmd" value="_s-xclick" />
-                      <input
-                        type="hidden"
-                        name="hosted_button_id"
-                        value="L8C3FR3RTLQG2"
-                      />
-                      <input type="hidden" name="currency_code" value="USD" />
-                      <input
-                        type="image"
-                        src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif"
-                        border="0"
-                        name="submit"
-                        title="PayPal - The safer, easier way to pay online!"
-                        alt="Buy Now"
-                      />
-                    </form>
+                    {/* TODO */}
                   </div>
                 )}
 
