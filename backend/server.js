74786eae7e4eabba4c7b35c4907ddea446d12ab2
Zekaria Mohammed <zakisu250@gmail.com>
2023-12-09 12:54:37 +0300

    Update pagination limit and add production build configuration

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/orders', orderRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -38,6 +34,18 @@ app.get('/api/config/paypal', (req, res) =>
 // make uploads folder static
 const __dirname = path.resolve();
 app.use('/upload', express.static(path.join(__dirname, '../frontend/public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
